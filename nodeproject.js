8fa4967b3214735ce0a0d1cc150a7f08ff5a3f40
karthik <karthikinfo2001@gmail.com>
2023-08-26 12:45:58 +0530

    This is 1 version

---
+++ nodeproject.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+// Listen on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
