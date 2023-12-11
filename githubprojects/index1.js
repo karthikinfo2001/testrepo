c7765e3189acc33dae123f9fd6bef2fe7fc12100
karthik <karthikinfo2001@gmail.com>
2023-12-11 18:18:51 +0530

    This is 1dec commit

---
+++ githubprojects/index1.js
@@ -0,0 +1,89 @@
+//var a=10
+//var b=20
+//console.log(a+b)
+
+
+/*{
+    let a=10
+    let b=20
+    console.log(a+b)  
+}
+*/
+
+/*const a=10
+const b=a*5
+console.log(b)
+*/
+
+//FUNCTIONS:
+
+/*function hi() 
+{
+    console.log("Hello")
+    console.log(typeof "karthik")
+}
+hi()
+*/
+
+//TASK FUNCTION:
+
+/*function iphone()
+{
+    console.log("This is iphone")
+}
+
+function redmi()
+{
+    console.log("This is redmi")
+}
+
+function samsung()
+{
+    console.log("This is samsung")
+}
+
+iphone()
+redmi()
+samsung()
+*/
+
+//FUNCTION WITH VAR TASK:
+/*
+var a=10
+var b=20
+function add()
+{
+    console.log(a+b)
+}
+
+add()
+*/
+
+//Task 2 in function:
+/*
+var factor="kamal"
+var fplayer="dhoni"
+var fmovie="anbe Sivam"
+*/
+/*function favourite()
+{
+    console.log(factor)
+    console.log(fplayer)
+    console.log(fmovie)
+}
+
+favourite()
+*/
+
+// IF & ELSE CONCEPT: 
+var climate="rain"
+
+if(climate === "rain")
+{
+    console.log("Enjoy the rain")
+}
+
+else
+{
+    console.log("Sorry not raining")
+}
